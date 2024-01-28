76d6afc54ad5fdc308072a3ce5c1cdcf567dc47e
yena <w2302@e-mirim.hs.kr>
2024-01-29 02:03:56 +0900

    Js 기초 const, console.log

---
+++ app.js
@@ -0,0 +1,11 @@
+//const=바뀌지 않는 값 (계속 유지됨)
+const a=5;
+const b=2;
+const myName="yena";
+alert("hi!");
+console.log("yena's Js") //""도 ''도 사용 가능 "" 안에 들어가는 것은 문자가 됨
+console.log(a+b);
+console.log(a*b);
+console.log(a-b);
+console.log(a/b);
+console.log("hello"+myName);

e7dc310d3c37606de07bf8878d5aa5f37cf026cc
yena <w2302@e-mirim.hs.kr>
2024-01-31 00:26:50 +0900

    array

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-//const=바뀌지 않는 값 (변수명임) (계속 유지됨)
-const a=5;
-const b=2;
-const myName="yena";
-alert("hi!");
-console.log("yena's Js") //""도 ''도 사용 가능 "" 안에 들어가는 것은 문자가 됨
-console.log(a+b);
-console.log(a*b);
-console.log(a-b);
-console.log(a/b);
-console.log("hello"+myName);
+// array 작성 법 = [](대괄호 사용), 각 항목 ,(쉼표)로 분리.
+//array의 목적 : 하나의 variable 안에 데이터의 list 가지는 것
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+//array에서 아이템 얻어오기
+console.log(daysOfWeek); //컴퓨터는 숫자를 0부터. -> mon=0번, tue=1번 ~~~
+
+//array에 요일 추가하기
+days0fWeek.push("sun")
+console.log(daysOfWeek); 
+
+const toBuy = ["potato", "apple", "tomato"]
+console.log(toBuy[2]); // tomato가 결과값으로 나옴. (만약 3이상의 숫자를 대괄호에 넣는다면 결과 = undefined)
+//undefined = 정의 되지 않음
+
+ 

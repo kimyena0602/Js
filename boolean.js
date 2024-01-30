0bff5f6dfdaa0e154ca37e0517db19864a3700e7
yena <w2302@e-mirim.hs.kr>
2024-01-31 00:03:41 +0900

    boolean 타입

---
+++ boolean.js
@@ -0,0 +1,6 @@
+//boolean + true, false가 있다.
+const anIFat = null; // "" <- 이거 사용 x
+//null = 그 변수에 아무것도 없음을 의미
+let something;
+console.log(amIFat); //amIFat의 값 = "비어있음"
+//undefind = variable이 메모리에 만들어졌지만 값이 없는 것을 뜻함.

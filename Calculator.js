d8e72c57c127e4221522a17a94240f9049c86925
yena <w2302@e-mirim.hs.kr>
2024-02-02 02:12:03 +0900

    Function

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+function plus (a, b){
+    console.log(a + b); // a = 8, b = 11
+}
+function divide(a,b){
+    console.log(a/b);
+}
+
+plus(8, 11);
+divide(125, 5);
+
--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-//object와 array의 차이점  
-//1. object는 중괄호를 사용한다.
-//2.array는 데이터타입이 어떠한 특성을 갖고 있는지 알기 어려웠다면 object는 한 눈에 확인 할 수 있다.
-const player = { 
-    //이 곳엔 property(특성)을 작성한다. (중괄호 안에는 =을 사용하지 않고 :을 사용한다.)
-    name : "yena", // 또 다른 특성을 작성하기 위해 작성문 끝에는 ,를 붙인다.
-    point : 30, 
-    fat : true, //다양한 데이터 타입 사용 가능
+//function = 반복해서 사용할 수 있는 코드조각
+function sayHello() {// fuction은 ()를 사용한다.
+    console.log("Hello");
 }
 
-console.log(player);
-player.lastName = "apple"; // lastName을 추가한거 처럼 object에 뭔가를 추가할 수 있음.
-console.log(player.name); // = console.log(player["name"]); (name을 불러오는 것)
+sayHello(); 
+
+//function에는 argument를 넣을 수 있다.
+//argument = function을 실행하는 동안 어떠한 정보를 function에게 전달하는 방법
+function sayHello(nameOfPerson, age){ //argument는 여러개 넣을 수 있다.
+    console.log("Hello my name is " + nameOfPerson + ", and i'm "+age);
+}
+sayHello("yena" , 18);
+sayHello("jina" , 16);
+sayHello("chaeunwoo" , 20);
+
+//player Object
+const player = {
+    name: "yena",
+    sayHello: function(PersonName){ // PersonName = yen, kim
+        console.log("hello! " + PersonName + " nice meet you,");
+    },
+};
+
+player.sayHello("yen");
+player.sayHello("kim");

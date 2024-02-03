869a3a8b1b6339dec410b8cde81ba100e432e5ca
yena <w2302@e-mirim.hs.kr>
2024-02-04 01:47:29 +0900

    function2

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-// array 작성 법 = [](대괄호 사용), 각 항목 ,(쉼표)로 분리.
-//array의 목적 : 하나의 variable 안에 데이터의 list 가지는 것
-const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+//object와 array의 차이점  
+//1. object는 중괄호를 사용한다.
+//2.array는 데이터타입이 어떠한 특성을 갖고 있는지 알기 어려웠다면 object는 한 눈에 확인 할 수 있다.
+const player = { 
+    //이 곳엔 property(특성)을 작성한다. (중괄호 안에는 =을 사용하지 않고 :을 사용한다.)
+    name : "yena", // 또 다른 특성을 작성하기 위해 작성문 끝에는 ,를 붙인다.
+    point : 30, 
+    fat : true, //다양한 데이터 타입 사용 가능
+}
 
-//array에서 아이템 얻어오기
-console.log(daysOfWeek); //컴퓨터는 숫자를 0부터. -> mon=0번, tue=1번 ~~~
+console.log(player);
+player.lastName = "apple"; // lastName을 추가한거 처럼 object에 뭔가를 추가할 수 있음.
+console.log(player.name); // = console.log(player["name"]); (name을 불러오는 것)
 
-//array에 요일 추가하기
-days0fWeek.push("sun")
-console.log(daysOfWeek); 
 
-const toBuy = ["potato", "apple", "tomato"]
-console.log(toBuy[2]); // tomato가 결과값으로 나옴. (만약 3이상의 숫자를 대괄호에 넣는다면 결과 = undefined)
-//undefined = 정의 되지 않음
-
- 

b14a178e35d137c163b23d3a134afb529cda3200
yena <w2302@e-mirim.hs.kr>
2024-02-03 21:20:26 +0900

    alert 이용해서 값 출력

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,23 @@
-function plus (a, b){
-    console.log(a + b); // a = 8, b = 11
-}
-function divide(a,b){
-    console.log(a/b);
-}
+const calculator = {
+    plus: function (a,b){
+        alert(a+b); //console.log 말고 alert로 주면 값이 하나씩 나옴
+    },
+    minus: function (a,b){
+        alert(a-b);
+    },
+    times: function(a,b){
+        alert(a*b);
+    },
+    divide: function(a,b){
+        alert(a/b);
+    },
+    power: function (a,b) {
+        alert(a**b);
+    },
+};
 
-plus(8, 11);
-divide(125, 5);
-
+calculator.plus(2,3);
+calculator.times(2,3);
+calculator.minus(2,3);
+calculator.divide(2,3);
+calculator.power(2,3);

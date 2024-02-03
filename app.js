9bb692796664a4671edb93b0989af4f37d1f7f56
yena <w2302@e-mirim.hs.kr>
2024-02-03 21:47:04 +0900

    return

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-//function = 반복해서 사용할 수 있는 코드조각
-function sayHello() {// fuction은 ()를 사용한다.
-    console.log("Hello");
+const age = 96; //96은 5번줄 age로 이동 -> 2번 줄 ageOfForeigner로 이동 -> 3번 줄에서 return 하여 98이라는 값이 나온다.
+function calculaterKrAge(ageOfForeigner) { //함수는 어떤 일을 수행하고 그 결과를 알려주는 것
+   return ageOfForeigner + 2; //return은 ageOfForeigner을 calculateKrage의 결과로 대체하는 역할
 }
-
-sayHello(); 
-
-//function에는 argument를 넣을 수 있다.
-//argument = function을 실행하는 동안 어떠한 정보를 function에게 전달하는 방법
-function sayHello(nameOfPerson, age){ //argument는 여러개 넣을 수 있다.
-    console.log("Hello my name is " + nameOfPerson + ", and i'm "+age);
-}
-sayHello("yena" , 18);
-sayHello("jina" , 16);
-sayHello("chaeunwoo" , 20);
-
-//player Object
-const player = {
-    name: "yena",
-    sayHello: function(PersonName){ // PersonName = yen, kim
-        console.log("hello! " + PersonName + " nice meet you,");
-    },
-};
-
-player.sayHello("yen");
-player.sayHello("kim");
+const krAge= calculaterKrAge(age);
+console.log(krAge);

abd9153ba13dac14fbfc7b645c4251b8f5d140db
yena <w2302@e-mirim.hs.kr>
2024-02-07 23:28:25 +0900

    js

--- 반복문 (for, while,do while).js
+++ 반복문 (for, while,do while).js
@@ -2,7 +2,6 @@
 //for (초기값; 조건문; 실행문){
 // 반복할 코드
 //}
-
 for (let i = 0; i < 0; i++){
     console.log(i);
 }
@@ -12,14 +11,12 @@ for (let i = 0; i < 0; i++){
 //while (조건문){ 
     //실행문
 //}
-
 let a = 0;
 while (i<10){
     a++
 }
 
 //do while문 (적어도 한 번은 실행함 while문과의 차이점)
-
 let b = 0;
 do {
     i++
@@ -27,7 +24,6 @@ do {
 
 //break : 멈추고 빠져나옴
 //continue : 멈추고 다음 반복으로 진행
-
 while (true){
     let answer = comfirm ('keepgoing?');
     if(!answer){

555b4f8f7d261bb4ed5a74e2bda6256a114d9088
yena <w2302@e-mirim.hs.kr>
2024-02-14 01:08:23 +0900

    string -> int 로

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// array 작성 법 = [](대괄호 사용), 각 항목 ,(쉼표)로 분리.
-//array의 목적 : 하나의 variable 안에 데이터의 list 가지는 것
-const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+//string -> int 로 바꿔주는 praseInt
 
-//array에서 아이템 얻어오기
-console.log(daysOfWeek); //컴퓨터는 숫자를 0부터. -> mon=0번, tue=1번 ~~~
+const age = prompt ("How old are you?");
 
-//array에 요일 추가하기
-days0fWeek.push("sun")
-console.log(daysOfWeek); 
-
-const toBuy = ["potato", "apple", "tomato"]
-console.log(toBuy[2]); // tomato가 결과값으로 나옴. (만약 3이상의 숫자를 대괄호에 넣는다면 결과 = undefined)
-//undefined = 정의 되지 않음
-
- 
+console.log (age , praseInt(age));

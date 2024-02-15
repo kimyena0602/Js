906093c11b3ec77ed9ef1273269d9b7a58104541
yena <w2302@e-mirim.hs.kr>
2024-02-16 01:15:54 +0900

    alert로 기본동작 막기

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
      event.preventDefault(); // preventDefault 함수란 어떤 event의 기본 행동이 발생되지 않도록 막는 함수
      console.log(loginInput.value);
 }
 
+function handleLinkClick () {
+    alert("clicked")
+}
+ 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-//submit event가 발생할 때 js는 onloginSubmit 함수를 호출 
+link.addEventListener("click", handleLinkClick);

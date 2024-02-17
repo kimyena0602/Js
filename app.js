e7c2ed1e89f5203d4cce3787da4dbf12f015d764
yena <w2302@e-mirim.hs.kr>
2024-02-18 00:12:29 +0900

    오류 고침

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const loginForm = document.querySelector("#login-form inout");
+const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
@@ -6,7 +6,7 @@ function onLoginBtnClick(){
     if(username === ""){
         alert("Please write your name.");
     } else if (username.length>15){
-        alert ("Your name is too long.")
+        alert ("Your name is too long.");
     }
 }
 loginButton.addEventListener("click", onLoginBtnClick);

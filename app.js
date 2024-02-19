9ff8c176f2e878f31ef1ba43cc99e5d9c0ad503d
yena <w2302@e-mirim.hs.kr>
2024-02-19 15:54:16 +0900

    css와 js

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor==="black"){
-        newColor = "tomato";
-    } else {
-        newColor = "black";
+    const clickedClass = "clicked";
+    if(h1.classList.contains (clickedClass)){
+        h1.classList.remove(clickedClass);
+    } else{
+        h1.classList.add(clickedClass);
     }
-    h1.style.color = newColor;
-}
-
-function handleMouseEnter (){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!"
 }
 
 function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
 }
 
 function handleWindowCopy(){
@@ -36,8 +25,6 @@ function handleWindowOnline(){
 }
 
 h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
 
 window.addEventListener("resize", handleWindowResize); //resize는 h1은 안됨. 
 window.addEventListener("copy", handleWindowCopy);

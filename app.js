e6326b39f6eb824a66097a893782e1730cffd4a4
yena <w2302@e-mirim.hs.kr>
2024-02-21 14:37:36 +0900

    if, else if 문과 &&연산자, ||연산자

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-//string -> int 로 바꿔주는 praseInt
+const age = parseInt(prompt("How old are you?"));
 
-const age = prompt ("How old are you?");
-
-console.log (age , praseInt(age));
+if(isNaN(age)){
+    console.log("Write a number.");
+}
+else if(age<18){
+    console.log("You can't drink.");
+}
+else if (age>=18 && age<=50){ //&&(AND연산자) = 두 가지 조건이 충족될 때 true값을 가져옴.
+    console.log("You can drink.");
+}
+else if(age>=18 || age>50){ // ||(OR연산자) = 두 가지 조건 중 하나라도 충족될 때 true 값을 가져옴.
+    console.log("You can drink.");
+}
+else {
+    console.log("You can't drink.")
+}

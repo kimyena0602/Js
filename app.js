5865f2fadc03b7df8271742c6a879081fcaa4bfe
yena <w2302@e-mirim.hs.kr>
2024-02-22 14:26:10 +0900

    html과 js 연결

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-if(isNaN(age) || age<0){
-    console.log("Write a number.");
+function handleTitleClick(){
+    console.log ("title was clicked!");
 }
-else if(age<18){
-    console.log("You can't drink.");
-}
-else if (age>=18 && age<=50){ //&&(AND연산자) = 두 가지 조건이 충족될 때 true값을 가져옴.
-    console.log("You can drink.");
-}
-else if(age>=18 || age>50){ // ||(OR연산자) = 두 가지 조건 중 하나라도 충족될 때 true 값을 가져옴.
-    console.log("You can drink.");
-}
-else {
-    console.log("You can't drink.")
-}
+title.addEventListener("click", handleTitleClick);

4d1df88d9d652886cd5541a4bfc9deb6a13e58d3
yena <w2302@e-mirim.hs.kr>
2024-02-24 02:29:29 +0900

    다양한 function과 함께

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1     = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     console.log ("title was clicked!");
+    h1.style.color = "black"
 }
-title.addEventListener("click", handleTitleClick);
+
+function handleMouseEnter (){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!"
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("No WIFI");
+}
+
+function handleWindowOnline(){
+    alert("Nice~!");
+}
+
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize); //resize는 h1은 안됨. 
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);

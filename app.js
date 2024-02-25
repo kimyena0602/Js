6f09d866918ce26adba9726754546ea692cee036
yena <w2302@e-mirim.hs.kr>
2024-02-26 01:07:05 +0900

    preventDefault

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-     console.log(username);
+function onLoginSubmit(event){
+     event.preventDefault(); // preventDefault 함수란 어떤 event의 기본 행동이 발생되지 않도록 막는 함수
+     console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
 
+//submit event가 발생할 때 js는 onloginSubmit 함수를 호출 

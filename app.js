dcdfcb1911c558d4cef5f7382c07d75e0c52b079
yena <w2302@e-mirim.hs.kr>
2024-02-26 01:41:56 +0900

    h1 조건에 맞게 숨기기

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; // 일반적으로 string만 포함된 변수는 대문자로 표기, string을 저장하고 싶을 때 사용
 
 function onLoginSubmit(event){
      event.preventDefault(); // preventDefault 함수란 어떤 event의 기본 행동이 발생되지 않도록 막는 함수
-     console.log(loginInput.value);
+     loginForm.classList.add(HIDDEN_CLASSNAME);
+     const username = loginInput.value;
+     greeting.innerText = "Hello " + username
+     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 function handleLinkClick () {
@@ -13,4 +17,3 @@ function handleLinkClick () {
 }
  
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);

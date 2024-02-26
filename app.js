4f3406fd382ad87c44de3ed8c6d78d221927c97d
yena <w2302@e-mirim.hs.kr>
2024-02-26 18:31:40 +0900

    localStorage를 알아보았다.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event){
      event.preventDefault(); // preventDefault 함수란 어떤 event의 기본 행동이 발생되지 않도록 막는 함수
      loginForm.classList.add(HIDDEN_CLASSNAME);
      const username = loginInput.value;
+     localStorage.setItem("username", username); 
      greeting.innerText = "Hello " + username 
      greeting.classList.remove(HIDDEN_CLASSNAME)
 }
@@ -17,3 +18,4 @@ function handleLinkClick () {
 }
  
 loginForm.addEventListener("submit", onLoginSubmit);
+//localStorage: 개발자 도구를 이용해 브라우저에 공짜로 무언가를 저장할 수 있게 하는 기능
